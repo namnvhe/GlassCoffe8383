f3b1c2f86a4d68abd2c5dabae46c8b7a4dd0518f
namnvhe <namlegend586@gmail.com>
2025-06-17 21:28:42 +0700

    CommitResetBranchVuDev

--- Cafe.BusinessObjects/Models/Response/ResetPasswordResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cafe.BusinessObjects.Models.Response
-{
-    public class ResetPasswordResponse
-    {
-        public bool IsSuccess { get; set; }
-        public string Message { get; set; } = string.Empty;
-        public List<string>? Errors { get; set; }
-    }
-}
